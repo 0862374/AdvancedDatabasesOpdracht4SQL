1e9d6c5054d2ed50c22afac228ff32b551a7a670
Nabil T <0862374@hr.nl>
2014-06-08 19:29:25 +0200

    made the case from 4.1 till 4.5

---
+++ src/opdrachtvier/DatabaseMy.java
@@ -0,0 +1,33 @@
+package opdrachtvier;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseMy {
+	final String DB_NAME = "opdrachtviersql";
+	final String DB_URL = "jdbc:mysql://localhost/" + DB_NAME;
+
+	// Database accountinstellingen
+	final String USER = "rootjava";
+	final String PASS = "1234";
+	public Connection conn = null;
+	public Statement stmt = null;
+
+	public DatabaseMy() {
+		try {
+			System.out.println("Connecting to database...");
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+			stmt = conn.createStatement();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Connecting to database failed...");
+			e.printStackTrace();
+
+		}
+		// printAllProducts();
+		// exampleTransaction();
+	}
+
+}
---
+++ src/opdrachtvier/OnbeveilingdeApp.java
@@ -0,0 +1,140 @@
+package opdrachtvier;
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class OnbeveilingdeApp {
+	JFrame loginScreen;
+	JFrame funOneScreen;
+	JFrame funTwoScreen;
+	JTextField username;
+	JTextField password;
+	JTextField klasname;
+
+	public OnbeveilingdeApp() {
+
+		loginScreen = new JFrame("Onbeveiligde app");
+		loginScreen.setLayout(new GridLayout(0, 1));
+
+		loginScreen.setVisible(true);
+		paintLogin();
+		loginScreen.pack();
+		loginScreen.repaint();
+
+	}
+
+	public void paintLogin() {
+		username = new JTextField(10);
+		password = new JTextField(10);
+		JButton btnlogin = new JButton();
+
+		loginScreen.add(new JLabel("Username:"));
+		loginScreen.add(username);
+		loginScreen.add(new JLabel("Password:"));
+		loginScreen.add(password);
+		loginScreen.add(btnlogin);
+
+		loginScreen.add(new JLabel());
+
+		btnlogin.setText("Log in");
+
+		btnlogin.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				checkLogin();
+			}
+		});
+	}
+
+	protected void checkLogin() {
+		// TODO Auto-generated method stub
+		DatabaseMy db = new DatabaseMy();
+
+		try {
+			String sql = "SELECT * FROM studenten INNER JOIN klassen ON stu_klas_id=klas_id WHERE stu_id = '"
+					+ username.getText() + "' AND stu_ww = '" + password.getText() + "'";
+			System.out.println(sql);
+			ResultSet rs = db.stmt.executeQuery(sql);
+			int rows = 0;
+			while (rs.next()) {
+				if (rows == 0) {
+					funOneScreen = new JFrame("Functionaliteit een");
+					funOneScreen.setLayout(new GridLayout(0, 1));
+					funOneScreen.setVisible(true);
+					funOneScreen.add(new JLabel("ID, Naam, Klas, Ingeschreven"));
+
+					funTwoScreen = new JFrame("Functionaliteit twee");
+					funTwoScreen.setLayout(new GridLayout(0, 1));
+					funTwoScreen.setVisible(true);
+					funTwoScreen.add(new JLabel("Klasnaam: "));
+					klasname = new JTextField(10);
+					funTwoScreen.add(klasname);
+					JButton btnklas = new JButton();
+					btnklas.setText("Zoek klassen op");
+					funTwoScreen.add(btnklas);
+					btnklas.addActionListener(new ActionListener() {
+
+						@Override
+						public void actionPerformed(ActionEvent arg0) {
+							// TODO Auto-generated method stub
+							getKlassen();
+						}
+					});
+				}
+				rows++;
+
+				funOneScreen.add(new JLabel(rs.getString("stu_id") + ", " + rs.getString("stu_naam") + ", "
+						+ rs.getString("klas_naam") + ", " + rs.getString("stu_ingeschreven")));
+
+				funOneScreen.pack();
+				funOneScreen.repaint();
+				funTwoScreen.pack();
+				funTwoScreen.repaint();
+
+			}
+			System.out.println("Number of rows found: " + rows);
+			if (rows == 0) {
+				loginScreen.add(new JLabel("Username and password is not correct."));
+				loginScreen.pack();
+				loginScreen.repaint();
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void getKlassen() {
+		DatabaseMy db = new DatabaseMy();
+		try {
+			String sql = "SELECT * FROM klassen INNER JOIN studenten ON stu_klas_id=klas_id WHERE stu_ingeschreven='Ja' AND klas_naam = '"
+					+ klasname.getText() + "'";
+			System.out.println(sql);
+			ResultSet rs;
+
+			rs = db.stmt.executeQuery(sql);
+			while (rs.next()) {
+				funTwoScreen.add(new JLabel(rs.getString("stu_id") + ", " + rs.getString("stu_naam") + ", "
+						+ rs.getString("klas_naam") + ", " + rs.getString("stu_ingeschreven")));
+
+				funTwoScreen.pack();
+				funTwoScreen.repaint();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+}
